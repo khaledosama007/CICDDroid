fc2bb73d86bbca3fedd8506e2261f2e6ee574656
Khaled Osama <khaledosama500@gmail.com>
2023-11-12 19:23:28 +0200

    Initial

---
+++ app/src/main/java/com/lalorosas/retirementcalculator/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.lalorosas.retirementcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        AppCenter.start(application, "15bc904d-11db-4ab0-979e-2246f74a9c4c", Analytics::class.java, Crashes::class.java);
+    }
+}
